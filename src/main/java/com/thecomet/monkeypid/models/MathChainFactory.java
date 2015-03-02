a59d7a4bce159b93cc9454be4baa68df1965c8d5
Alex Murray <alexander.murray@students.fhnw.ch>
2015-03-02 21:45:42 +0100

    Added documentation to the PID controller math chain factory

--- src/main/java/com/thecomet/monkeypid/models/MathChainFactory.java
+++ src/main/java/com/thecomet/monkeypid/models/MathChainFactory.java
@@ -4,11 +4,37 @@ import com.thecomet.monkeypid.interfaces.MathBlock;
 import com.thecomet.monkeypid.interfaces.MathBlockInterface;
 
 public class MathChainFactory {
-    
-    public static MathBlockInterface pidController(double p, double i, double d) {
+
+    /**
+     * Generates a math chain implementing a PID controller and error difference block.
+     *
+     * Visualisation of this math chain:
+     *              Error block     P block        I block       D block
+     *                 _____        _______        _______        _______
+     * Target   ------| +   |      |       |      |   /   |      | /\    |        Return value of
+     *                |     |------| ______|------|  /    |------||  |   |------  block.stepAll()
+     * Current  ------| -   |      ||      |      | /     |      |/  \___|
+     * block.set()     -----        -------        -------        -------
+     *
+     * The target value can be set with block.stepAll(targetValue, deltaTime).
+     *
+     * Note how there is no feedback loop (the loop is open). This is so other math
+     * chains can be inserted in between, if necessary. The loop can be closed by
+     * passing a "current" value into block.set().
+     *
+     * Valid math block names for getBlock() are:
+     *  + "error" for the error block.
+     *  + "pid" for the PID controller block.
+     *
+     * @param kp The factor to use for the P block.
+     * @param ki The factor to use for the I block.
+     * @param kd The factor to use for the D block.
+     * @return Returns a non-integrated value representing the correction to be taken.
+     */
+    public static MathBlockInterface pidController(double kp, double ki, double kd) {
         return new MathBlock("error") {
             private double error;
-            // error
+
             @Override
             public double stepSingle(double inputValue, double deltaTime) {
                 return (inputValue - this.error) * deltaTime;
@@ -17,16 +43,15 @@ public class MathChainFactory {
                 this.error = value;
             }
         }.setNextBlock(new MathBlock("pid") {
-            // PID
-            private double kp = p, ki = i, kd = d;
+            private double factorP = kp, factorI = ki, factorD = kd;
             private double valueI;
             private double lastError;
 
             @Override
             public double stepSingle(double error, double deltaTime) {
-                double p = error * kp;
-                this.valueI += error * ki;
-                double d = (this.lastError - error) * kd;
+                double p = error * factorP;
+                this.valueI += error * factorI;
+                double d = (this.lastError - error) * factorD;
                 this.lastError = error;
                 return (p + this.valueI + d) / 3.0;
             }
@@ -65,7 +90,7 @@ public class MathChainFactory {
                 // step system - use result from controller as input for the system
                 double feedbackResult = system.stepAll(controllerResult, deltaTime);
                 // feedback result from system into the controller, thus closing the control loop
-                controller.getBlock("error").set(feedbackResult);
+                controller.set(feedbackResult);
 
                 return feedbackResult;
             }
