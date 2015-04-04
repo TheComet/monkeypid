b11179c19ec004afbc4c00eed92543f04e0f0d00
Alex Murray <alexander.murray@students.fhnw.ch>
2015-04-04 22:28:23 +0200

    Wrote some sample code, showing the basics of how to use JFreeChart

--- src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/GraphPanel.java
+++ src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/GraphPanel.java
@@ -1,6 +1,18 @@
 package ch.fhnw.ht.eit.pro2.team3.monkeypid;
 
-import java.awt.Dimension;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.StandardXYItemRenderer;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.data.DomainOrder;
+import org.jfree.data.general.DatasetChangeListener;
+import org.jfree.data.general.DatasetGroup;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,12 +25,38 @@ public class GraphPanel extends JPanel implements ActionListener {
 	// Eingabefeld Ks Tu Tg
 	private JLabel lbEingabeTitel = new JLabel(
 			"Darstellung des Graphen");
-	
 
 	public GraphPanel(Controller controller) {
 		super(new GridBagLayout());
-		
 
+        // Create a test series of data
+        XYSeries series = new XYSeries("Test Series");
+        series.add(0, 2);
+        series.add(1, 4);
+        series.add(2, 3);
+        series.add(3, 5);
+
+        // add series to collection (collection derives from XYDataset)
+        XYSeriesCollection data = new XYSeriesCollection();
+        data.addSeries(series);
+
+        // renderer
+        XYItemRenderer renderer = new StandardXYItemRenderer();
+
+        // axis
+        NumberAxis yAxis = new NumberAxis("Test Range");
+
+        // create plot
+        XYPlot plot = new XYPlot(data, null, yAxis, renderer);
+
+        // add plot into a new chart
+        JFreeChart chart = new JFreeChart("Test Plot", JFreeChart.DEFAULT_TITLE_FONT, plot, true);
+
+        // need a panel to add the chart to
+        ChartPanel panel = new ChartPanel(chart);
+
+        // finally, add panel as an element in our GraphPanel
+        this.add(panel);
 	}
 
 	/*public static void main(String args[]) {
