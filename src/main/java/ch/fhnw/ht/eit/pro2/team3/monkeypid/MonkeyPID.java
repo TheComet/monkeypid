f0baca11ed1b9a07636c6fa8f55db471fcc5abb6
Alex Murray <alexander.murray@students.fhnw.ch>
2015-04-10 15:01:13 +0200

    Trying to fix look and feel stuff

--- src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/MonkeyPID.java
+++ src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/MonkeyPID.java
@@ -1,25 +1,23 @@
 package ch.fhnw.ht.eit.pro2.team3.monkeypid;
 
 import javax.swing.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
 
-public class MonkeyPID extends JFrame {
+public class MonkeyPID {
 
-    public void init() {
-
+    public static void main(String[] args) {
         try {
             UIManager
                     .setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
         } catch (Exception exception) {
             exception.printStackTrace();
         }
-        
-        setUndecorated(true);
-        getRootPane().setWindowDecorationStyle(JRootPane.FRAME);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setTitle("Dimensionierungstool Phasengang-Methode");
+
+        JFrame frame = new JFrame();
+        frame.setUndecorated(true);
+        frame.getRootPane().setWindowDecorationStyle(JRootPane.FRAME);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Dimensionierungstool Phasengang-Methode");
 
         // initialise MVC stuff
         Model model = new Model();
@@ -27,10 +25,11 @@ public class MonkeyPID extends JFrame {
         TopViewPanel view = new TopViewPanel(controller, model);
         controller.setView(view);
         model.addObserver(view);
-        add(view);
-    }
 
-    public static void main(String[] args) {
+        frame.getContentPane().add(view);
+        frame.setVisible(true);
+
+        /*
         MonkeyPID app = new MonkeyPID();
 
         // handle window close event
@@ -44,7 +43,7 @@ public class MonkeyPID extends JFrame {
         // init app and run
         app.init();
         app.setVisible(true);
-        app.setTitle("Monkey PID");
+        app.setTitle("Monkey PID");*/
     }
 
     // TODO Josua Stierli - Remove
