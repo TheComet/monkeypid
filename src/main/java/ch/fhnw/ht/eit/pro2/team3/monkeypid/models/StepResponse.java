a49529f3d59571c4d54773191003903064072e68
Alex Murray <alexander.murray@students.fhnw.ch>
2015-04-22 13:37:51 +0200

    fixing runtime error in example curve calculation

--- src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/models/StepResponse.java
+++ src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/models/StepResponse.java
@@ -7,7 +7,7 @@ public class StepResponse {
 
     public static XYSeries exampleCalculate() {
 
-        PolynomialSplineFunction function = SaniCurves.get().getTuTgRatioCurve(2);
+        PolynomialSplineFunction function = SaniCurves.get().getTgInverseCurve(2);
         // construct XY dataset from the loaded data
         XYSeries series = new XYSeries("Test");
         for(double x = 0.0; x < 0.98; x += 0.01) {
