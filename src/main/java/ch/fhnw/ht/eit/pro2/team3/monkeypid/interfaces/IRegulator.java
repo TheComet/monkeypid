7d5c2b3ed9829823fec3eb4080d6832d488d61ee
Alex Murray <alexander.murray@students.fhnw.ch>
2015-04-22 15:00:04 +0200

    Default values for controller parameters is 0.0

--- src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/interfaces/IRegulator.java
+++ src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/interfaces/IRegulator.java
@@ -3,9 +3,9 @@ package ch.fhnw.ht.eit.pro2.team3.monkeypid.interfaces;
 public interface IRegulator {
 
     public class ControllerParameters {
-        public double kr;
-        public double tu;
-        public double tv;
+        public double kr = 0.0;
+        public double tu = 0.0;
+        public double tv = 0.0;
     }
 
     public ControllerParameters getParameters();
