5c8070a452f3267f889cd6d8256f6f3b282d524c
Alex Murray <alexander.murray@students.fhnw.ch>
2015-04-23 21:04:09 +0200

    Removing old code

--- src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/MonkeyPID.java
+++ src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/MonkeyPID.java
@@ -8,53 +8,8 @@ import ch.fhnw.ht.eit.pro2.team3.monkeypid.views.View;
 
 import javax.swing.*;
 
-import java.awt.Dimension;
-
 public class MonkeyPID extends JFrame {
 
-	// TODO Josua Stierli - Remove
-    /*public void init() {
-
-        try {
-            UIManager
-                    .setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-        
-        setUndecorated(true);
-        getRootPane().setWindowDecorationStyle(JRootPane.FRAME);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setTitle("Dimensionierungstool Phasengang-Methode");
-
-        // initialise MVC stuff
-        Model model = new Model();
-        Controller controller = new Controller(model);
-        TopViewPanel view = new TopViewPanel(controller, model);
-        controller.setView(view);
-        model.addObserver(view);
-        add(view);
-    }*/
-
-    /*public static void main(String[] args) {
-        MonkeyPID app = new MonkeyPID();
-
-        // handle window close event
-        app.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent windowEvent) {
-                System.exit(1);
-            }
-        });
-
-        // init app and run
-        app.init();
-        app.setVisible(true);
-        app.setTitle("Monkey PID");
-    }*/
-	
-
-    
     public static void main(String args[]) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
