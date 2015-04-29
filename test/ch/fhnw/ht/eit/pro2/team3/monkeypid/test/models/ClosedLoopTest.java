b6c05d384fba4e80af39285e7616f79ac8e7874e
Alex Murray <alex.murray@gmx.ch>
2015-04-29 16:07:44 +0200

    Adding example unit test case, for testing

---
+++ test/ch/fhnw/ht/eit/pro2/team3/monkeypid/test/models/ClosedLoopTest.java
@@ -0,0 +1,14 @@
+package ch.fhnw.ht.eit.pro2.team3.monkeypid.test.models;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClosedLoopTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
