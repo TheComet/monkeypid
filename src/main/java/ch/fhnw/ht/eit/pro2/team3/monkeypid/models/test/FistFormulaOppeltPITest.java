a5c776da55775be27b312b7b5b9a6f632f7c3a0f
Simon Sturm <simon.sturm@students.fhnw.ch>
2015-04-29 18:01:56 +0200

    Added Test for OppeltPI

---
+++ src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/models/test/FistFormulaOppeltPITest.java
@@ -0,0 +1,27 @@
+package ch.fhnw.ht.eit.pro2.team3.monkeypid.models.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ch.fhnw.ht.eit.pro2.team3.monkeypid.interfaces.ControllerCalculator;
+import ch.fhnw.ht.eit.pro2.team3.monkeypid.models.FistFormulaOppeltPI;
+import ch.fhnw.ht.eit.pro2.team3.monkeypid.models.Plant;
+
+public class FistFormulaOppeltPITest {
+
+	@Test
+	public void testCalculate() {
+		ControllerCalculator calc=new FistFormulaOppeltPI();
+		Plant plant=new Plant(1,1,1);
+		calc.calculate(plant);
+		calc.getController
+		assertEquals("OppeltPI Test failed",calc.getController(),); //Insert Matlab Value
+	}
+
+	@Test
+	public void testGetController() {
+		fail("Not yet implemented");
+	}
+
+}
