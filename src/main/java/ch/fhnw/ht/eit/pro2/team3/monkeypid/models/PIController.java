4f152557d87c585f3b3704c179686189a5ac3ada
Alex Murray <alexander.murray@students.fhnw.ch>
2015-05-04 18:20:51 +0200

    Remove unused import

--- src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/models/PIController.java
+++ src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/models/PIController.java
@@ -1,7 +1,5 @@
 package ch.fhnw.ht.eit.pro2.team3.monkeypid.models;
 
-import ch.fhnw.ht.eit.pro2.team3.monkeypid.interfaces.IController;
-
 import javax.swing.table.DefaultTableModel;
 
 public class PIController extends AbstractController {
