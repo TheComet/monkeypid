0a870c0ec8edb436e6f23d62b3b7a4b1507a6f1e
Alex Murray <alexander.murray@students.fhnw.ch>
2015-05-04 15:55:44 +0200

    Splitting up tests in zellweger PI

--- src/test/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/models/ZellwegerPITest.java
+++ src/test/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/models/ZellwegerPITest.java
@@ -11,30 +11,30 @@ public class ZellwegerPITest {
     SaniCurves sani = new SaniCurves();
 
     @Test
-    public void testCalculate() throws Exception {
-        IControllerCalculator cc;
-        PIController c;
-
-        // calculate zellweger PI with Tu=2, Tg=6, Ks=1, angleOfInflection=45°
-        cc = new ZellwegerPI(new Plant(2, 6, 1, sani), 45);
+    public void testCalculateTu2Tg6Ks1phi45() throws Exception {
+        IControllerCalculator cc = new ZellwegerPI(new Plant(2, 6, 1, sani), 45);
         cc.calculate();
-        c = (PIController)cc.getController();
+        PIController c = (PIController) cc.getController();
 
         assertEquals(3.3122, c.getTn(), delta);
         assertEquals(0.984, c.getKr(), delta);
+    }
 
-        // calculate zellweger PI with Tu=2, Tg=19, Ks=3, angleOfInflection=45°
-        cc.setPlant(new Plant(2, 19, 3, sani));
+    @Test
+    public void testCalculateTu2Tg19Ks3phi45() throws Exception {
+        IControllerCalculator cc = new ZellwegerPI(new Plant(2, 19, 3, sani), 45);
         cc.calculate();
-        c = (PIController)cc.getController();
+        PIController c = (PIController) cc.getController();
 
         assertEquals(6.541, c.getTn(), delta);
         assertEquals(0.5453, c.getKr(), delta);
+    }
 
-        // calculate zellweger PI with Tu=2, Tg=19, Ks=3, angleOfInflection=76.3°
-        cc = new ZellwegerPI(new Plant(2, 19, 3, sani), 76.3);
+    @Test
+    public void testCalculateTu2Tg19Ks3phi76_3() throws Exception {
+        IControllerCalculator cc = new ZellwegerPI(new Plant(2, 19, 3, sani), 76.3);
         cc.calculate();
-        c = (PIController)cc.getController();
+        PIController c = (PIController)cc.getController();
 
         assertEquals(6.541, c.getTn(), delta);
         assertEquals(0.0624, c.getKr(), delta);
