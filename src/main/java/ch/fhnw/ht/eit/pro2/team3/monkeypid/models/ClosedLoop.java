2ade8b0fca0176b918f44def690a338e12fb398f
Alex Murray <alexander.murray@students.fhnw.ch>
2015-05-05 16:07:05 +0200

    Moving stuff from TransferFunctionClosedLoop to ClosedLoop's calculateStepResponse() method, still a WIP

--- src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/models/ClosedLoop.java
+++ src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/models/ClosedLoop.java
@@ -1,7 +1,11 @@
 package ch.fhnw.ht.eit.pro2.team3.monkeypid.models;
 
 import ch.fhnw.ht.eit.pro2.team3.monkeypid.interfaces.IController;
+import ch.fhnw.ht.eit.pro2.team3.monkeypid.services.MathStuff;
+import org.apache.commons.math3.complex.Complex;
 import org.jfree.data.xy.XYSeries;
+
+import java.util.Arrays;
 
 public class ClosedLoop {
 
@@ -14,6 +18,46 @@ public class ClosedLoop {
     }
 
     public XYSeries calculateStepResponse() {
+        /*
+        B = MathStuff.conv(hS.getNumeratorCoefficients(), hR.getNumeratorCoefficients());
+        A = MathStuff.conv(hS.getDenominatorCoefficients(), hR.getDenominatorCoefficients());
+
+        for (int i = 0; i < B.length ; i++) {
+            A[A.length - B.length + i] += B[i];
+        }
+
+        schrittIfft(new TransferFunction(A, B), 100, 10);
+
+        double fs = 100;
+        int N = 1024;
+
+        double [] omega = MathStuff.linspace(0, fs * Math.PI, N / 2);
+
+        // calculate frequency response
+        Complex[] H = MathStuff.freqs(g, omega);
+
+        // calculate impulse response
+        H = MathStuff.symmetricMirrorConjugate(H);
+        Complex[] h = MathStuff.ifft(H);
+
+        // calculate step response - note that h doesn't have an
+        // imaginary part, so we can use conv as if it were a double
+        double[] y = MathStuff.conv(MathStuff.real(h), MathStuff.ones(N+1));
+
+        // cut away mirrored part
+        y = Arrays.copyOfRange(y, 0, y.length / 2);
+
+        // generate time axis
+        double[] t = MathStuff.linspace(0, (y.length-1)/fs, y.length);
+
+        // create XY data series for jfreechart
+        XYSeries series = new XYSeries(""); // TODO name?
+        for(int i = 0; i < t.length; i++) {
+            series.add(t[i], y[i]);
+        }
+
+        return series;*/
+
         return null;
     }
 
