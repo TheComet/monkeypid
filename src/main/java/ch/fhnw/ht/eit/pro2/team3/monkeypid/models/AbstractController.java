47b1f24591fefc78cd48cd33a9b934e20241eac4
Alex Murray <alexander.murray@students.fhnw.ch>
2015-05-05 16:05:54 +0200

    Controllers now have transfer functions

--- src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/models/AbstractController.java
+++ src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/models/AbstractController.java
@@ -4,13 +4,23 @@ import ch.fhnw.ht.eit.pro2.team3.monkeypid.interfaces.IController;
 
 public abstract class AbstractController implements IController{
     private String name;
+    private TransferFunction transferFunction;
 
     public AbstractController(String name) {
         this.name = name;
     }
 
     @Override
-    public String getName() {
+    public final String getName() {
         return name;
+    }
+
+    @Override
+    public final TransferFunction getTransferFunction() {
+        return transferFunction;
+    }
+
+    protected final void setTransferFunction(TransferFunction transferFunction) {
+        this.transferFunction = transferFunction;
     }
 }
