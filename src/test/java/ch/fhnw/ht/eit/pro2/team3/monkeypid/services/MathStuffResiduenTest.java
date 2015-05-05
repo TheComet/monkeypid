761fd2c22fbe2fab57cc3dbf1ffb97b71068b346
Simon Wyss <simon.wyss2@students.fhnw.ch>
2015-05-05 10:44:15 +0200

    changed location of Residuen Test Class to src/test

--- src/test/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/services/MathStuffResiduenTest.java
+++ src/test/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/services/MathStuffResiduenTest.java
@@ -5,10 +5,12 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MathStuffResiduenTest {
+	double[] B = {4.0812, 1.1400};
+	double[] A = {7.4902, 33.6613, 46.4843, 23.2772, 7.6612, 1.1400};
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
 	}
 
 }
