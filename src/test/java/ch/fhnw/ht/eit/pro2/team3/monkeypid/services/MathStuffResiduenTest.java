0b20d3a4118fe07a0dd61379f2cc4658ceae2257
Simon Wyss <simon.wyss2@students.fhnw.ch>
2015-05-06 00:16:58 +0200

    testResidueSimpleItSelfe() not all test ok, some are wrong

--- src/test/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/services/MathStuffResiduenTest.java
+++ src/test/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/services/MathStuffResiduenTest.java
@@ -6,6 +6,8 @@ import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.math3.complex.Complex;
 import org.junit.Test;
 
+import ch.fhnw.ht.eit.pro2.team3.monkeypid.models.TransferFunction;
+
 public class MathStuffResiduenTest {
 	double[] B = {4.0812, 1.1400};
 	double[] A = {7.4902, 33.6613, 46.4843, 23.2772, 7.6612, 1.1400};
@@ -78,17 +80,14 @@ public class MathStuffResiduenTest {
 		
 		System.out.println("myRoots");
 		for (int i = 0; i < myRoots.length; i++) {
-			System.out.println("Real: "+myRoots[i].getReal() +" Imag: " +myRoots[i].getImaginary());
+			//System.out.println("Real: "+myRoots[i].getReal() +" Imag: " +myRoots[i].getImaginary());
 		}
-		
-		
 		
 		assertEquals( -2.3190, myRoots[0].getReal(), delta);
 		assertEquals(-1.5915, myRoots[1].getReal(), delta);
 		assertEquals(-0.1529, myRoots[2].getReal(), delta);
 		assertEquals(-0.2777, myRoots[3].getReal(), delta);
 		assertEquals(-0.1529, myRoots[4].getReal(), delta);
-		
 		
 		assertEquals(+ 0.0000, myRoots[0].getImaginary(), delta);
 		assertEquals(+ 0.0000, myRoots[1].getImaginary(), delta);
@@ -112,7 +111,7 @@ public class MathStuffResiduenTest {
 		
 		System.out.println("myPolyComplex");
 		for (int i = 0; i < pa.length; i++) {
-			System.out.println("Real: "+pa[i].getReal() +" Imag: " +pa[i].getImaginary());
+			//System.out.println("Real: "+pa[i].getReal() +" Imag: " +pa[i].getImaginary());
 		}
 		
 		assertEquals( 1.0, pa[0].getReal(), delta);
@@ -130,6 +129,25 @@ public class MathStuffResiduenTest {
 		assertEquals(0.0, pa[4].getImaginary(), delta);
 		assertEquals(0.0, pa[5].getImaginary(), delta);
 		
+	}
+	
+	@Test
+	public void testResidueSimpleItSelfe(){
+		Object[] myObs = MathStuff.residueSimple(new TransferFunction(B, A));
+		Complex[] R = (Complex[]) myObs[0];
+		Complex[] P = (Complex[]) myObs[1];
+		double K = (double) myObs[2];
+		System.out.println("ResidueSimple:");
+		//only value of first cell correct
+		System.out.println("R:");
+		for (int i = 0; i < R.length; i++) {
+			System.out.println("Real: "+R[i].getReal() +" Imag: " +R[i].getImaginary());
+		}
+		//only some cell values correct
+		System.out.println("P:");
+		for (int i = 0; i < P.length; i++) {
+			System.out.println("Real: "+P[i].getReal() +" Imag: " +P[i].getImaginary());
+		}
 	}
 	
 }
