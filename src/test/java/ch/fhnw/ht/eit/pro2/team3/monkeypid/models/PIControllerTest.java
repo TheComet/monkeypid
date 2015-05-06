fd40622ec3ce82d1f933ed114aaed257feb7f3bb
Simon Wyss <simon.wyss2@students.fhnw.ch>
2015-05-06 09:18:20 +0200

    added tests for PIController

--- src/test/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/models/PIControllerTest.java
+++ src/test/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/models/PIControllerTest.java
@@ -5,11 +5,38 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class PIControllerTest {
-
+	double delta = 1.5e-4;
+	
 	@Test
-    public void testPIControllerGetTransferFunction()  throws Exception {
-		double test = 1.0;
-		assertEquals(test, 1, 0.1);
+    public void testPIControllerConstructor()  throws Exception {
+			//[Tn, Kr] = p2_zellweger_pi_tu_tg(45-180,2,6,1)
+		//Tn = 3.3122, Kr =    0.9840
+		double Kr = 0.9840;
+		double Tn = 3.3122;
+		PIController myPIController = new PIController("myPI", Kr, Tn);
+		
+		double[] NumeratorExpected = {3.2591, 0.9840};
+		double[] DenominatorExpected = {3.3122, 0.0};
+		assertArrayEquals(NumeratorExpected, myPIController.getTransferFunction().getNumeratorCoefficients(),delta);
+		assertArrayEquals(DenominatorExpected, myPIController.getTransferFunction().getDenominatorCoefficients(),delta);
+		
+		
+	}
+	
+	@Test
+    public void testPIControllerSetParameters()  throws Exception {
+			//[Tn, Kr] = p2_zellweger_pi_tu_tg(45-180,2,6,1)
+		//Tn = 3.3122, Kr =    0.9840
+		double Kr = 0.9840;
+		double Tn = 3.3122;
+		
+		PIController myPIController = new PIController("myPI", 1.2, 5.5);
+		myPIController.setParameters(Kr, Tn);
+		
+		double[] NumeratorExpected = {3.2591, 0.9840};
+		double[] DenominatorExpected = {3.3122, 0.0};
+		assertArrayEquals(NumeratorExpected, myPIController.getTransferFunction().getNumeratorCoefficients(),delta);
+		assertArrayEquals(DenominatorExpected, myPIController.getTransferFunction().getDenominatorCoefficients(),delta);
 	}
 
 }
