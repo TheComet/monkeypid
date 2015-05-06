6cf5d6eec89b3db3cc03fc719af71883fcb1b455
Alex Murray <alexander.murray@students.fhnw.ch>
2015-05-06 10:37:18 +0200

    Writing unit tests for transfer function, and hopefully this commit fixes a compilation error

--- src/test/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/models/ClosedLoopTest.java
+++ src/test/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/models/ClosedLoopTest.java
@@ -2,21 +2,26 @@ package ch.fhnw.ht.eit.pro2.team3.monkeypid.models;
 
 import static org.junit.Assert.*;
 
+import ch.fhnw.ht.eit.pro2.team3.monkeypid.interfaces.IController;
 import org.junit.Test;
 
 public class ClosedLoopTest {
-    double[] Bs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    double[] Br = {11, 12, 13, 14};
-    double[] As = {15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
-    double[] Ar = {25, 26, 27, 28, 29};
+    private SaniCurves sani = new SaniCurves();
+    private double[] bs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    private double[] br = {11, 12, 13, 14};
+    private double[] as = {15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
+    private double[] ar = {25, 26, 27, 28, 29};
 
     @Test
     public void testTransferFunctionClosedLoop() {
-        TransferFunctionClosedLoop tFCL= new TransferFunctionClosedLoop(
-                new TransferFunction(As, Bs),
-                new TransferFunction(Ar, Br)
-        );
-        //assertArrayEquals(, actuals);
-    }
+        Plant plant = new Plant(2, 6, 1, sani);
+        IController controller = new PIController("test", 1, 1);
 
+        plant.getTransferFunction().setNumeratortorCoefficients(bs);
+        plant.getTransferFunction().setDenominatorCoefficients(as);
+        controller.getTransferFunction().setNumeratortorCoefficients(br);
+        controller.getTransferFunction().setDenominatorCoefficients(ar);
+
+        ClosedLoop loop = new ClosedLoop(plant, controller);
+    }
 }
