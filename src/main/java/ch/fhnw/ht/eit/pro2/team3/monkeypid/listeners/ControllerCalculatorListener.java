90430c75e2afa5b4819a1eba87667444bb891fc4
Alex Murray <alexander.murray@students.fhnw.ch>
2015-05-08 18:12:45 +0200

    Documented ControllerCalculatorListener

--- src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/listeners/ControllerCalculatorListener.java
+++ src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/listeners/ControllerCalculatorListener.java
@@ -2,6 +2,14 @@ package ch.fhnw.ht.eit.pro2.team3.monkeypid.listeners;
 
 import ch.fhnw.ht.eit.pro2.team3.monkeypid.models.AbstractControllerCalculator;
 
+/**
+ * Allows classes to register as listeners to the ControllerCalculator class.
+ */
 public interface ControllerCalculatorListener {
+
+    /**
+     * This is called when the calculator has completed calculating a controller for a given plant.
+     * @param calculator The calculator.
+     */
     void onControllerCalculationComplete(AbstractControllerCalculator calculator);
 }
