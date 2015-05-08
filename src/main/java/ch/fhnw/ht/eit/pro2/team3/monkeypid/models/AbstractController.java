3c7483a3de643b36951b573961f732aaa293985b
Alex Murray <alexander.murray@students.fhnw.ch>
2015-05-08 18:36:08 +0200

    Documented AbstractController

--- src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/models/AbstractController.java
+++ src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/models/AbstractController.java
@@ -2,36 +2,86 @@ package ch.fhnw.ht.eit.pro2.team3.monkeypid.models;
 
 import java.awt.*;
 
+/**
+ * Represents the controller component in a closed loop system. The controller can have multiple types, e.g. I, PI,
+ * or PID. This is a base class defining the common interface.
+ *
+ * Each controller defines a name, a colour, and most importantly, a transfer function. The way the transfer function
+ * is calculated depends on the controller type and is taken care of in the deriving classes.
+ *
+ * Once a controller is calculated, it can be connected to a Plant object using a ClosedLoop object.
+ * @author Alex Murray
+ */
 public abstract class AbstractController {
     private String name;
     private Color color;
     private TransferFunction transferFunction;
 
+    /**
+     * Constructor. The name of the controller is required and should be unique among all calculations within a
+     * simulation. In this implementation, we set it to the name of the method used to calculate the controller.
+     * @param name A string identifying this controller. This is used as a key for many things, so it must be unique.
+     */
     public AbstractController(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the name of this controller.
+     * @return A string containing the name of the controller.
+     */
     public final String getName() {
         return name;
     }
 
+    /**
+     * Gets the colour of this controller. The colour is used for the colour of the curve in the chart, and for
+     * colouring the checkboxes and rows in the table.
+     * @return A colour object.
+     */
     public final Color getColor() {
         return color;
     }
 
+    /**
+     * Sets the colour of this controller. The colour is used for the colour of the curve in the chart, and for
+     * colouring the checkboxes and rows in the table.
+     * @param color A new colour object.
+     */
     public final void setColor(Color color) {
         this.color = color;
     }
 
+    /**
+     * Returns the transfer function of this controller. There are multiple ways to calculate the transfer function, as
+     * it depends on the controller type (I, PI, PID). See the derived classes for more info.
+     * @return The transfer function of this controller.
+     */
     public final TransferFunction getTransferFunction() {
         return transferFunction;
     }
 
+    /**
+     * Sets the transfer function of this controller. Only derived classes should be able to set the transfer function,
+     * because it doesn't really make sense for anything else outside to know how to calculate it.
+     * @param transferFunction
+     */
     protected final void setTransferFunction(TransferFunction transferFunction) {
         this.transferFunction = transferFunction;
     }
 
+    /**
+     * Must be implemented by derived classes. This should calculate the transfer function of the controller and then
+     * pass it to setTransferFunction().
+     */
     protected abstract void calculateTransferFunction();
 
+    /**
+     * Should return an array of strings to insert into the table of results. The table is designed to contain all
+     * result values of a PID controller in the following order:
+     *     new String[] {"Controller Name", "Kr", "Tn", "Tv", "Tp"};
+     * If your controller is missing some of these parameters, then you should specify empty strings in their place.
+     * @return The length of the string array must be 5.
+     */
     public abstract String[] getTableRowStrings();
 }
