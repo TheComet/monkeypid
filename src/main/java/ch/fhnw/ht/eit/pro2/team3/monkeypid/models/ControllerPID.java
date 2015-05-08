3467458ac5b2919510884483e56e710f4c7ef7b9
Alex Murray <alexander.murray@students.fhnw.ch>
2015-05-08 20:06:56 +0200

    Documentation for ControllerPID

--- src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/models/ControllerPID.java
+++ src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/models/ControllerPID.java
@@ -2,6 +2,10 @@ package ch.fhnw.ht.eit.pro2.team3.monkeypid.models;
 
 import java.text.DecimalFormat;
 
+/**
+ * Implements a PID controller. This consists of the controller parameters Kr, Tn, Tv, and Tp, and a transfer function.
+ * @author Alex Murray
+ */
 public class ControllerPID extends AbstractController {
 
     private double kr = 0.0;
@@ -9,11 +13,69 @@ public class ControllerPID extends AbstractController {
     private double tv = 0.0;
     private double tp = 0.0;
 
+    /**
+     * Constructs a new PI controller from the parameters Kr and Tn and calculates the transfer function.
+     * @param name A unique name for this controller. Must be unique for all controllers in a simulation.
+     * @param tn The controller parameter Tn.
+     * @param tv The controller parameter Tv.
+     * @param kr The controller parameter Kr.
+     * @param tp The controller parameter Tp.
+     */
     public ControllerPID(String name, double tn, double tv, double kr, double tp) {
         super(name);
         setParameters(tn, tv, kr, tp);
     }
 
+    /**
+     * Sets the parameters Tn, Tv, Kr, and Tp for the PID controller and calculates the transfer function.
+     * @param tn The controller parameter Tn.
+     * @param tv The controller parameter Tv.
+     * @param kr The controller parameter Kr.
+     * @param tp The controller parameter p.
+     */
+    public void setParameters(double tn, double tv, double kr, double tp) {
+        this.tn = tn;
+        this.tv = tv;
+        this.kr = kr;
+        this.tp = tp;
+        calculateTransferFunction();
+    }
+
+    /**
+     * Gets the controller parameter Tn.
+     * @return Tn.
+     */
+    public double getTn() {
+        return tn;
+    }
+
+    /**
+     * Gets the controller parameter Tv.
+     * @return Tv.
+     */
+    public double getTv() {
+        return tv;
+    }
+
+    /**
+     * Gets the controller parameter Kr.
+     * @return Kr.
+     */
+    public double getKr() {
+        return kr;
+    }
+
+    /**
+     * Gets the controller parameter Tp.
+     * @return Tp.
+     */
+    public double getTp() {
+        return tp;
+    }
+
+    /**
+     * Calculates the transfer function for a PID controller.
+     */
     @Override
     protected void calculateTransferFunction() {
         // Numerator and Denominator Poly of the pid-controller:
@@ -28,6 +90,10 @@ public class ControllerPID extends AbstractController {
         );
     }
 
+    /**
+     * Creates an array of strings to be inserted into the table in the GUI.
+     * @return Array of strings of the length 5.
+     */
     @Override
     public String[] getTableRowStrings() {
         return new String[]{
@@ -37,29 +103,5 @@ public class ControllerPID extends AbstractController {
                 new DecimalFormat("0.000E0").format(getTv()),
                 new DecimalFormat("0.000E0").format(getTp())
         };
-    }
-
-    public void setParameters(double tn, double tv, double kr, double tp) {
-        this.tn = tn;
-        this.tv = tv;
-        this.kr = kr;
-        this.tp = tp;
-        calculateTransferFunction();
-    }
-
-    public double getTn() {
-        return tn;
-    }
-
-    public double getTv() {
-        return tv;
-    }
-
-    public double getKr() {
-        return kr;
-    }
-
-    public double getTp() {
-        return tp;
     }
 }
