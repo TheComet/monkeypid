7606b58be6009c460e2421595819c9b0f145e810
Alex Murray <alexander.murray@students.fhnw.ch>
2015-05-08 18:11:25 +0200

    Documented ClosedLoopListener

--- src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/listeners/ClosedLoopListener.java
+++ src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/listeners/ClosedLoopListener.java
@@ -3,8 +3,14 @@ package ch.fhnw.ht.eit.pro2.team3.monkeypid.listeners;
 import ch.fhnw.ht.eit.pro2.team3.monkeypid.models.ClosedLoop;
 
 /**
- *
+ * Allows classes to register as listeners to the ClosedLoop class.
+ * @author Alex Murray
  */
 public interface ClosedLoopListener {
+
+    /**
+     * This is called when the closed loop finishes calculating the step response.
+     * @param closedLoop The closed loop object that completed the step response calculation.
+     */
     void onStepResponseCalculationComplete(ClosedLoop closedLoop);
 }
