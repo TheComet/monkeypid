baf25953b0ff1b47515c4ec57cdd3ab905036769
Alex Murray <alexander.murray@students.fhnw.ch>
2015-05-08 20:00:10 +0200

    Documentation for ControllerI

--- src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/models/ControllerI.java
+++ src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/models/ControllerI.java
@@ -2,15 +2,44 @@ package ch.fhnw.ht.eit.pro2.team3.monkeypid.models;
 
 import java.text.DecimalFormat;
 
+/**
+ * Implements an I controller. This consists of the controller parameter Ti and a transfer function.
+ * @author Alex Murray
+ */
 public class ControllerI extends AbstractController {
 
     private double ti;
 
+    /**
+     * Constructs a new I controller from the parameter Ti and calculates the transfer function.
+     * @param name A unique name for this controller. Must be unique for all controllers in a simulation.
+     * @param ti The parameter Ti.
+     */
     public ControllerI(String name, double ti) {
         super(name);
         setParameters(ti);
     }
 
+    /**
+     * Sets the parameter Ti for the I controller and calculates the transfer function.
+     * @param ti The parameter Ti.
+     */
+    public final void setParameters(double ti) {
+        this.ti = ti;
+        calculateTransferFunction();
+    }
+
+    /**
+     * Gets the controller parameter Ti.
+     * @return Ti.
+     */
+    public final double getTi() {
+        return ti;
+    }
+
+    /**
+     * Calculates the transfer function for this controller.
+     */
     @Override
     protected void calculateTransferFunction() {
         // Numerator and Denominator Poly of the pi-controller:
@@ -24,6 +53,10 @@ public class ControllerI extends AbstractController {
         );
     }
 
+    /**
+     * Creates an array of strings to be inserted into the table in the GUI.
+     * @return Array of strings of the length 5.
+     */
     @Override
     public String[] getTableRowStrings() {
         return new String[]{
@@ -33,14 +66,5 @@ public class ControllerI extends AbstractController {
                 "", // Tv  (PID only)     -- need to pad so overswing value has the correct offset in the table
                 ""  // Tp  (PID only)
         };
-    }
-
-    public void setParameters(double ti) {
-        this.ti = ti;
-        calculateTransferFunction();
-    }
-
-    public double getTi() {
-        return ti;
     }
 }
