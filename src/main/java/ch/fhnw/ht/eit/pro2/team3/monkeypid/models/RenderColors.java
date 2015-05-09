c7c18ae934b61d4611b4c01d5edf6b8f44f9b61b
Alex Murray <alexander.murray@students.fhnw.ch>
2015-05-09 02:59:27 +0200

    Documentation for RenderColors

--- src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/models/RenderColors.java
+++ src/main/java/ch/fhnw/ht/eit/pro2/team3/monkeypid/models/RenderColors.java
@@ -2,6 +2,12 @@ package ch.fhnw.ht.eit.pro2.team3.monkeypid.models;
 
 import java.awt.*;
 
+/**
+ * A global list of the colours of each calculator. This makes it easier to have an overview and manage the
+ * colours. The calculators will return these in their getColor() methods, and the chart will render each controller
+ * according to these colours.
+ * @author Alex Murray
+ */
 public class RenderColors {
     public static final Color OPPELT_PI               = new Color(0, 100, 0);
     public static final Color OPPELT_PID              = new Color(0, 100, 0);
